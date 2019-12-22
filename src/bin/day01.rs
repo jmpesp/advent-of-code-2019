df641aa7fb8021c0e055874f6734363520b54caf
James MacMahon <jwm@operand.ca>
2019-12-22 14:55:51 -0500

    solve day 1 part 2

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -5,6 +5,15 @@ use std::convert;
 // Specifically, to find the fuel required for a module, take its mass,
 // divide by three, round down, and subtract 2.
 
+// part 2:
+// Fuel itself requires fuel just like a module - take its mass, divide
+// by three, round down, and subtract 2. However, that fuel also
+// requires fuel, and that fuel requires fuel, and so on. Any mass that
+// would require negative fuel should instead be treated as if it
+// requires zero fuel; the remaining mass, if any, is instead handled by
+// wishing really hard, which has no mass and is outside the scope of
+// this calculation.
+
 fn get_fuel(mass: i32) -> i32 {
     return mass / 3 - 2;
 }
@@ -15,6 +24,21 @@ fn test_get_fuel() {
     assert_eq!(get_fuel(14), 2);
     assert_eq!(get_fuel(1969), 654);
     assert_eq!(get_fuel(100756), 33583);
+}
+
+fn get_fuel_part_2(mass: i32) -> i32 {
+    let fuel = get_fuel(mass);
+    if (fuel < 0) {
+        return 0;
+    }
+    return fuel + get_fuel_part_2(fuel);
+}
+
+#[test]
+fn test_get_fuel_part_2() {
+    assert_eq!(get_fuel_part_2(14), 2);
+    assert_eq!(get_fuel_part_2(1969), 966);
+    assert_eq!(get_fuel_part_2(100756), 50346);
 }
 
 fn main() {
@@ -28,7 +52,7 @@ fn main() {
     let mut fuel: i32 = 0;
 
     for i in 0..numbers.len() {
-        fuel += get_fuel(numbers[i]);
+        fuel += get_fuel_part_2(numbers[i]);
     }
 
     println!("{}", fuel);
